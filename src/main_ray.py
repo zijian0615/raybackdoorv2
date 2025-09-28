3d2f11abadfef5491f48be81cd5ab0d70ae3c4a9
zijian0615 <156042420+zijian0615@users.noreply.github.com>
2025-09-27 22:26:15 -0500

    Update main_ray.py

--- src/main_ray.py
+++ src/main_ray.py
@@ -41,7 +41,7 @@ class PoisonedReconActorCPU:
         self.vae.eval()
 
     def run_pipeline(self, dataset_root="/home/ray/raybackdoorv1/data", target_label=0, trigger_size=3,
-                     gradcam_layer='layer3', gradcam_threshold=0.8, batch_size=4):
+                     gradcam_layer='layer3', gradcam_threshold=0.8, batch_size=16):
         # 在 Actor 内重新构建 DataLoader
         transform = transforms.Compose([
             transforms.ToTensor(),
