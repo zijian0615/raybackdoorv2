6fec6ec0e73ee6f492e5354379f0fb4ad1d751f4
zijian0615 <156042420+zijian0615@users.noreply.github.com>
2025-10-02 10:30:02 -0500

    Increase batch size from 32 to 128

--- runcifar10.py
+++ runcifar10.py
@@ -41,7 +41,7 @@ testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform
 )
 
-batch_size = 32
+batch_size = 128
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=batch_size, shuffle=False, num_workers=2
 )
