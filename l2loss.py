51ba6b3afa15fddb96baaa6c9544c2253f821f7b
zijian0615 <156042420+zijian0615@users.noreply.github.com>
2025-10-15 17:15:33 -0500

    Add CIFAR-10 dataset loading functionality

--- l2loss.py
+++ l2loss.py
@@ -23,6 +23,11 @@ transform_train = transforms.Compose([
 ])
 
 # CIFAR-10 数据集
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
+                                          shuffle=True, num_workers=2)
+
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform_train)
 testloader = torch.utils.data.DataLoader(testset, batch_size=128,
